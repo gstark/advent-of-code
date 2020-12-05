b557b6318f1a8ec64a750b5d56c921891ea59a79
Gavin Stark <gavin@gstark.com>
2020-12-05 17:19:49 -0500

    2020/05/part1.rb

---
+++ 2020/05/part1.rb
@@ -0,0 +1,9 @@
+passes = STDIN.read.split("\n")
+
+def bin_search(string, range, low, high)
+  string.chars.reduce(range) { |result, letter| mid = (result.max - result.min) / 2 + result.min; letter == low ? (result.min..mid) : (mid + 1..result.max) }.min
+end
+
+seats = passes.map { |pass| 8 * bin_search(pass[0..6], (0..127), "F", "B") + bin_search(pass[7..9], (0..7), "L", "R") }
+
+p seats.max
