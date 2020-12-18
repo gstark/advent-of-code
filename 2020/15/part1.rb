9c22b884e9f6c805a8855ba6956dc937fd5abdda
Gavin Stark <gavin@gstark.com>
2020-12-18 02:13:21 -0500

    2020/15/part1.rb

---
+++ 2020/15/part1.rb
@@ -0,0 +1,15 @@
+first_seen = {}
+last_seen = {}
+
+input = [0, 1, 5, 10, 3, 12, 19]
+
+input.each.with_index { |say, index| last_seen[say] = index + 1 }
+
+answer = (input.length + 1..2020).reduce(input.last) do |last_number, turn|
+  (first_seen[last_number] ? last_seen[last_number] - first_seen[last_number] : 0).tap do |say|
+    first_seen[say] = last_seen[say]
+    last_seen[say] = turn
+  end
+end
+
+p answer
