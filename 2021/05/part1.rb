f6d4d96904df953f50a3bdb6898ba14232ead380
Gavin Stark <gavin@gstark.com>
2021-12-06 09:53:06 -0500

    Day 5 - Part 1

---
+++ 2021/05/part1.rb
@@ -0,0 +1,101 @@
+require 'awesome_print'
+# 0,9 -> 5,9
+
+lines = STDIN.read.split("\n").map { |line| line.split("->").map { |parts| parts.split(",").map(&:to_i) } }
+
+# Need to start with only the horizontal or vertical lines
+
+
+# [[0, 9], [5, 9]]
+filtered_lines = lines.select { |(x1,y1), (x2,y2)| x1 == x2 || y1 == y2 }
+
+# (0,0)
+# .......1..
+# ..1....1..
+# ..1....1..
+# .......1..
+# .112111211
+# ..........
+# ..........
+# ..........
+# ..........
+# 222111....
+#           (9,9)
+
+
+# [
+#   [0, 0, 0, 0, 0, 0, 1, 0, 0]
+#   [0, 0, 1, 0, 0, 0, 1, 0, 0]
+#   [0, 0, 1, 0, 0, 0, 1, 0, 0]
+#   [0, 0, 0, 0, 0, 0, 1, 0, 0]
+#   [0, 0, 0, 0, 0, 0, 1, 0, 0]
+#   [0, 0, 0, 0, 0, 0, 0, 0, 0]
+#   [0, 0, 0, 0, 0, 0, 0, 0, 0]
+#   [0, 0, 0, 0, 0, 0, 0, 0, 0]
+#   [2, 2, 2, 1, 1, 1, 0, 0, 0]
+# ]
+
+# {
+#   [0, 6] => 1
+#   [1, 6] => 1
+#   [2, 6] => 1
+# }
+
+# Start with an empty hash of positions and counts
+hash = {}
+# Loop through our filtered lines
+filtered_lines.each do |(x1,y1),(x2,y2)|
+  # For each line
+  #   loop through each position in that line
+  if x1 == x2
+    range = y1 < y2 ? (y1..y2) : (y2..y1)
+
+    range.each do |y|
+      position = [x1,y]
+
+      #   - If that position isn't in our hash, then add it to the hash with a count of 0
+      if !hash.include?(position)
+        hash[position] = 0
+      end
+
+      #   - Increment the value at the position in the hash
+      hash[position] += 1
+    end
+  end
+
+  if y1 == y2
+    range = x1 < x2 ? (x1..x2) : (x2..x1)
+
+    range.each do |x|
+      position = [x, y1]
+
+      #   - If that position isn't in our hash, then add it to the hash with a count of 0
+      if !hash.include?(position)
+        hash[position] = 0
+      end
+
+      #   - Increment the value at the position in the hash
+      hash[position] += 1
+    end
+  end
+end
+
+
+# To get our answer count the number of values where the value is two or more
+ap hash.values.count { |value| value > 1}
+
+
+
+
+# 0,9    1,9   2,9    3,9    4,9    5,9
+
+# 7,0 
+
+# 7,1
+# 7,2
+# 7,3
+
+# 7,4
+
+
+
