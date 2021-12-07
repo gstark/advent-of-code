0b9dad4145a299ef3ceefa0f5f1e16004e0a32d5
Gavin Stark <gavin@gstark.com>
2021-12-07 12:13:56 -0500

    Day 07 - Part 1

---
+++ 2021/07/part1.rb
@@ -0,0 +1,20 @@
+require 'awesome_print'
+
+positions = $stdin.read.split(",").map(&:to_i)
+
+# 16,1,2,0,4,2,7,1,2,14
+
+# Cost of moving all to 16
+# 16 - 16 = 0
+# 16 - 1  = 15
+# 16 - 2  - 14
+# ...
+#
+# Cost of moving all to 1
+# 16 - 1 = 15
+# 1  - 1 = 0
+# 2  - 1 = 1
+# ...
+
+fuel_total = positions.map { |to| positions.sum { |from| (from - to).abs } }.min
+ap fuel_total
