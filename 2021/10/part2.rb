1996af2b3cc51c18451cf915ce23d7ab95bb6bdd
Gavin Stark <gavin@gstark.com>
2021-12-10 17:28:30 -0500

    Day 10 - Refactor - Uses a lookup hash instead of a character sequence

--- 2021/10/part2.rb
+++ 2021/10/part2.rb
@@ -2,17 +2,27 @@ require 'awesome_print'
 
 lines = $stdin.read.split("\n").map(&:chars)
 
-OPENS =  "({<[".chars
-CLOSES = ")}>]".chars
-POINTS = { ")" => 1, "]" => 2, "}" => 3, ">" => 4 }
+PAIRS = {
+  "(" => ")",
+  "{" => "}",
+  "<" => ">",
+  "[" => "]"
+}
+
+POINTS = {
+  ")" => 1,
+  "]" => 2,
+  "}" => 3,
+  ">" => 4
+}
 
 ap lines.map { |line|
      line.each.with_object({stack: [], illegal: nil}) { |character, result|
        break result if result[:illegal]
 
-       if OPENS.include?(character)
-         result[:stack] << character
-       elsif CLOSES.index(character) == OPENS.index(result[:stack].last)
+       if PAIRS[character]
+         result[:stack].push(character)
+       elsif PAIRS[result[:stack].last] == character
          result[:stack].pop
        else
          result[:illegal] = character
@@ -20,7 +30,7 @@ ap lines.map { |line|
      }
    }
    .reject { |result| result[:illegal] }
-   .map { |result| result[:stack].reverse.map { |character| CLOSES[OPENS.index(character)] } }
+   .map { |result| result[:stack].reverse.map(&PAIRS) }
    .map { |line| line.reduce(0) { |total, character| total = total * 5 + POINTS[character] } }
    .sort
    .yield_self { |list| list[list.length/2] }
