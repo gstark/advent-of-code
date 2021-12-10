9f84406c6ae787aa3541fa5906f19e0bdd6cf49a
Gavin Stark <gavin@gstark.com>
2021-12-10 09:36:54 -0500

    Day 9 - Part 2

---
+++ 2021/09/part2.rb
@@ -0,0 +1,42 @@
+require 'awesome_print'
+require 'set'
+
+heights = $stdin.read.
+                 split("\n").
+                 map(&:chars).
+                 map.
+                 with_index { |row, row_index| row.map.with_index { |height, col_index| [[row_index, col_index], height.to_i] } }.
+                 flatten(1).
+                 to_h
+
+# basin size is 1 + sum of the basin size of all the non-nine-elements-around me
+
+def basin_size(row, col, heights, visited = Set.new)
+  return 0 if heights[[row,col]].nil? ||
+              visited.include?([row,col]) ||
+              heights[[row,col]] == 9
+
+  visited << [row, col]
+
+  1 + basin_size(row-1, col+0, heights, visited) +
+      basin_size(row+1, col+0, heights, visited) +
+      basin_size(row+0, col-1, heights, visited) +
+      basin_size(row+0, col+1, heights, visited)
+end
+
+low_points = heights.
+    select { |(row, col), height|
+              [
+                heights[[row-1, col+0]],
+                heights[[row+1, col+0]],
+
+                heights[[row+0, col-1]],
+                heights[[row+0, col+1]],
+              ].compact.all? { |neighbor| neighbor > height }
+            }
+
+ap low_points.
+     map { |(row, col), height| basin_size(row, col, heights) }.
+     sort.
+     last(3).
+     reduce(:*)
