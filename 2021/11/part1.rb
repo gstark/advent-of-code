e895b723a727b4e6087c6cc8393b374b493675ad
Gavin Stark <gavin@gstark.com>
2021-12-11 11:25:28 -0500

    Refactor the code to use an enumerator
    This allows us to abstract the stopping state to the calling code
    
    For instance in step 1 we 'take' 100 steps and use what is returned while in part 2 we keep looking at generated output until all the levels are zero
    
    This makes the code for the two parts nearly identical and is a more flexible solution

--- 2021/11/part1.rb
+++ 2021/11/part1.rb
@@ -15,49 +15,62 @@ def show_map(octo)
   puts octo.reduce([]) { |map, ((r,c), level)| (map[r] ||= [])[c] = level; map }.map { |row| row.join }.join("\n")
 end
 
-# Keep track of all the flashes
-all_flashes = []
-100.times do |step|
-  # Increase everyone's level
-  octo.transform_values!(&:succ)
+# This enumerator yields a hash of
+# {
+#   octo: the current map of the world
+#   flashes: the number of flashes seen
+#   step: the current step number
+# }
+step_enumerator = Enumerator.new do |yielder|
+  next_octo = octo.dup
+  total_flashes = 0
 
-  # Keep track of who has flashed already
-  already_flashed = []
+  (1..).each do |step|
+    # Increase everyone's level
+    next_octo.transform_values!(&:succ)
 
-  loop do
-    # Get the keys of the elements with levels more then 9 then remove any that have already flashed this round
-    new_flashers = octo
-                     .select { |(r,c), level| level > 9 }
-                     .keys - already_flashed
+    # Keep track of who has flashed already
+    already_flashed = []
 
-    # Stop processing this step if there aren't any new flashers
-    break if new_flashers.empty?
+    loop do
+      # Get the keys of the elements with levels more then 9 then remove any that have already flashed this round
+      new_flashers = next_octo
+                      .select { |(r,c), level| level > 9 }
+                      .keys - already_flashed
 
-    # Increment all the levels of the neighbors if they exist
-    new_flashers.each do |r,c|
-      [
-        [r-1,c-1],  # Above left
-        [r-1,c+0],  # Above
-        [r-1,c+1],  # Above right
+      # Stop processing this step if there aren't any new flashers
+      break if new_flashers.empty?
 
-        [r+0,c-1],  # Left
-        [r+0,c+1],  # Right
+      # Increment all the levels of the neighbors if they exist
+      new_flashers.each do |r,c|
+        [
+          [r-1,c-1],  # Above left
+          [r-1,c+0],  # Above
+          [r-1,c+1],  # Above right
 
-        [r+1,c-1],  # Below left
-        [r+1,c+0],  # Below
-        [r+1,c+1],  # Below right
-      ].each { |r,c| octo[[r,c]] +=1 if octo[[r,c]] }
+          [r+0,c-1],  # Left
+          [r+0,c+1],  # Right
+
+          [r+1,c-1],  # Below left
+          [r+1,c+0],  # Below
+          [r+1,c+1],  # Below right
+        ].each { |r,c| next_octo[[r,c]] +=1 if octo[[r,c]] }
+      end
+
+      # Keep track of the new flashers
+      already_flashed.concat(new_flashers)
+      total_flashes += new_flashers.length
     end
 
-    # Keep track of the new flashers
-    already_flashed.concat(new_flashers)
-    all_flashes.concat(new_flashers)
-  end
+    # Anyone over 9 is now a 0
+    next_octo.transform_values! { |level| level > 9 ? 0 : level }
 
-  # Anyone over 9 is now a 0
-  octo.transform_values! { |level| level > 9 ? 0 : level }
+    yielder << { octo: next_octo, flashes: total_flashes, step: step}
+  end
 end
 
-show_map(octo)
-puts
-puts all_flashes.size
+# Run 100 cycles of the enumerator
+state = step_enumerator.take(100).last
+
+show_map(state[:octo])
+puts "Flashes: #{state[:flashes]}"
