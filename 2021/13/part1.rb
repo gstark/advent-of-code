55fe404c1e6b71f1a65a6de859b4e45b9b05d344
Gavin Stark <gavin@gstark.com>
2021-12-13 17:53:59 -0500

    Day 13 - Refactoring to use OpenStruct

--- 2021/13/part1.rb
+++ 2021/13/part1.rb
@@ -1,24 +1,29 @@
+require 'ostruct'
 require 'awesome_print'
+
+def make_dot(x,y)
+  OpenStruct.new(x: x, y: y)
+end
 
 def print_dots(dots)
   max_x = dots.map { |dot| dot[:x] }.max
   max_y = dots.map { |dot| dot[:y] }.max
 
-  puts (0..max_y).map { |y| (0..max_x).map { |x| dots.include?({x: x, y: y}) ? "#" : "." }.join }
+  puts (0..max_y).map { |y| (0..max_x).map { |x| dots.include?(make_dot(x, y)) ? "#" : "." }.join }
 end
 
 dots = []
 folds = []
 $stdin.readlines.each do |line|
   case
-  when line =~ /(\d+),(\d+)/  then dots << { x: $1.to_i, y: $2.to_i }
+  when line =~ /(\d+),(\d+)/  then dots << make_dot($1.to_i, $2.to_i)
   when line =~ /([xy])=(\d+)/ then folds << [$1, $2.to_i]
   end
 end
 
 folds.take(1).each do |direction, offset|
-  dots = direction == "y" ? dots.map { |dot| dot[:y] > offset ? { x: dot[:x], y: 2 * offset - dot[:y] } : dot }
-                          : dots.map { |dot| dot[:x] > offset ? { x: 2 * offset - dot[:x], y: dot[:y] } : dot }
+  dots = direction == "y" ? dots.map { |dot| dot.y > offset ? make_dot(dot.x, 2 * offset - dot.y) : dot }
+                          : dots.map { |dot| dot.x > offset ? make_dot(2 * offset - dot.x, dot.y) : dot }
 end
 
 puts dots.uniq.size
