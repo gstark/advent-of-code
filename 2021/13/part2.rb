1c97da9b8bf8e2abe003fab8ffff53034e89aaec
Gavin Stark <gavin@gstark.com>
2021-12-13 17:45:23 -0500

    Day 13 - Refactoring

--- 2021/13/part2.rb
+++ 2021/13/part2.rb
@@ -7,16 +7,26 @@ def print_dots(dots)
   puts (0..max_y).map { |y| (0..max_x).map { |x| dots.include?({x: x, y: y}) ? "#" : "." }.join }
 end
 
-dots, folds = $stdin.read.split("\n").chunk_while { |a,b| !b.empty? }.to_a
-
-dots = dots.map { |dot| dot.split(",").map(&:to_i) }.map { |x,y| {x: x, y: y} }
-folds = folds.reject(&:empty?).map { |fold| fold.gsub(/fold along /, "") }.map { |fold| fold.split("=") }.map { |direction, offset| [direction, offset.to_i]}
-
-direction, offset = folds.first
+# Parse the input.
+#
+# We'll read the whole input, line by line.
+#
+# We'll consider each line and if it matches the dot pattern, we'll add it to the first element of
+# the dots_and_folds array, otherwise if it matches the fold pattern, we'll add it to the second.
+#
+# Then we'll destructure that array to get our dots and folds
+dots, folds = $stdin.read.split("\n").each.with_object([[], []]) { |line, dots_and_folds|
+  case
+  when line =~ /(\d+),(\d+)/
+    dots_and_folds.first << {x: $1.to_i, y: $2.to_i }
+  when line =~ /([xy])=(\d+)/
+    dots_and_folds.last << [$1, $2.to_i]
+  end
+}
 
 folds.each do |direction, offset|
-  dots = direction == "y" ? dots.map { |dot| dot[:y] > offset ? { x: dot[:x], y: 2 * offset - dot[:y] } : dot }.reject { |dot| dot[:y] == offset }
-                          : dots.map { |dot| dot[:x] > offset ? { x: 2 * offset - dot[:x], y: dot[:y] } : dot }.reject { |dot| dot[:x] == offset }
+  dots = direction == "y" ? dots.map { |dot| dot[:y] > offset ? { x: dot[:x], y: 2 * offset - dot[:y] } : dot }
+                          : dots.map { |dot| dot[:x] > offset ? { x: 2 * offset - dot[:x], y: dot[:y] } : dot }
 end
 
 print_dots(dots)
