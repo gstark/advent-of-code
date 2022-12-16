d9803b1adc6844bfe7c99689ef9b5ef82a92cd16
Gavin Stark <gavin@gstark.com>
2022-12-16 07:07:16 -0500

    Day 08 with better syntax
    See the improvement by using the `then` clause to chain after the find_index to remove weird logic about find_index being zero based

--- 2022/08/part2.rb
+++ 2022/08/part2.rb
@@ -7,15 +7,15 @@ answer = (0...size).flat_map do |row|
     me = trees[row][col]
 
     # Look each direction for the first index where we have a tree our size or taller
-    # since find index is 0 based we'll add one to the result. However, if we DON'T find
-    # anything find_index returns nil so we need to return one LESS than the size so we
-    # get the correct value when we add one. Curious if there is a way to avoid this +1 / -1
-    # magic.
+    # Uses `then` to see if the index is present, if so we'll use one MORE than that value
+    # since `find_index` is zero based. Otherwise we'll use the number of entries.
+    #
+    # This is cleaner than the previous hackery seen in the last commit.
     [
-      ((row - 1).downto(0).find_index { |r2| trees[r2][col] >= me } || row - 1) + 1,
-      ((col - 1).downto(0).find_index { |c2| trees[row][c2] >= me } || col - 1) + 1,
-      ((col + 1...size).find_index { |c2| trees[row][c2] >= me } || size - col - 2) + 1,
-      ((row + 1...size).find_index { |r2| trees[r2][col] >= me } || size - row - 2) + 1
+      (row - 1).downto(0).find_index { |r2| trees[r2][col] >= me }.then { |index| index ? index + 1 : row },
+      (col - 1).downto(0).find_index { |c2| trees[row][c2] >= me }.then { |index| index ? index + 1 : col },
+      (col + 1...size).find_index { |c2| trees[row][c2] >= me }.then { |index| index ? index + 1 : size - col - 1 },
+      (row + 1...size).find_index { |r2| trees[r2][col] >= me }.then { |index| index ? index + 1 : size - row - 1 }
     ].reduce(&:*)
   end
 end
