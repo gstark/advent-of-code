ccf03e689a838f8517d80db757ae96f7b2ce5656
Gavin Stark <gavin@gstark.com>
2023-06-05 23:29:47 -0400

    Using depth-first-search and removing 0 flow rate valves

--- 2022/16/part1.rb
+++ 2022/16/part1.rb
@@ -1,24 +1,38 @@
 require_relative 'astar'
+
 lines = ARGF.readlines(chomp: true).map { |line| line.scan(/Valve (..) has flow rate=(\d*); tunnels? leads? to valves? (.*)/).first }
 valves = lines.map { |valve, rate, other| {name: valve, rate: rate.to_i, neighbors: other.split(", ")} }
 
-distances = valves.to_h { |valve| [[valve[:name], valve[:name]], 0] }
-valves.combination(2).each do |a, b|
+@distances = {}
+valves.select { |valve| valve[:name] == "AA" || valve[:rate] > 0 }.combination(2).each do |a, b|
+  next if a[:name] == b[:name]
+
   result = a_star(start: a[:name], goal: b[:name], neighbors: proc { |valve_name| valves.find { |valve| valve[:name] == valve_name }[:neighbors] }, heuristic: proc { |a,b| 1 }, weight: proc { |a,b| 1 }, inspector: proc {}, visit: proc {})
 
-  distances[[a[:name], b[:name]]] = result[:path].length - 1
-  distances[[b[:name], a[:name]]] = result[:path].length - 1
+  @distances[[a[:name], b[:name]]] = result[:path].length - 1
+  @distances[[b[:name], a[:name]]] = result[:path].length - 1
 end
 
-remaining = 30
-loop do
-  max = valves.max_by { |valve|
-    distance = distances[["DD", valve[:name]]]
-    rate = valves.find { |o| o[:name] == valve[:name] }[:rate]
+@valve_rates = valves.to_h { |valve| [valve[:name], valve[:rate]] }
 
-    # p [valve[:name], (remaining - distance - 1), rate, (remaining - distance - 1) * rate * valve[:neighbors].length ]
-    (remaining - distance - 1) * rate * valve[:neighbors].length
-  }
-  valves.find { |valve| valve[:name] == max[:name] }[:rate] = 0
-  p max
+def dfs(current, flow_so_far, time_remaining, visited)
+  visited << current
+
+  max_flow = flow_so_far
+  neighbors = @distances.select { |(from, _to)| from == current }
+  neighbors.each do |(pair, distance)|
+    _from, to = pair
+
+    new_time_remaining = time_remaining - distance - 1
+
+    next if visited.include?(to) || new_time_remaining <= 0
+
+    new_flow = flow_so_far + @valve_rates[to] * (time_remaining - distance - 1) 
+
+    max_flow = [max_flow, dfs(to, new_flow, new_time_remaining, visited.dup)].max
+  end
+
+  max_flow
 end
+
+p dfs("AA", 0, 30, Set.new)
