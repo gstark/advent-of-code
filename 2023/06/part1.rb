2e4b8c64e1da594cdbd323ed78b2443146022c36
Gavin Stark <gavin@gstark.com>
2023-12-06 07:33:00 -0500

    2023 06 part 2 - Math approach

--- 2023/06/part1.rb
+++ 2023/06/part1.rb
@@ -1,7 +1,16 @@
+# A more direct way is to solve the equation
+#
+# -speed^2 +/- 7*speed - 9 > 0
+#
+# So we'll use quadratic formula to determine the high and low range
+#
 p STDIN.readlines
   .map { |line| line.split.map(&:to_i) }
   .map { |line| line[1..] }
   .yield_self { |lines| lines.first.zip(lines.last) }
   .map { |(time, distance)|
-    time.times.map { |speed| [speed, (time-speed)*speed] }.reject { |speed, new_distance| new_distance <= distance }.count
+    # Brute force:
+    # time.times.map { |speed| [speed, (time-speed)*speed] }.reject { |speed, new_distance| new_distance <= distance }.count
+
+    ((-time - Math.sqrt(time**2 - 4*distance))/(-2)).floor + 1 - ((-time + Math.sqrt(time**2 - 4*distance))/(-2)).ceil
   }.reduce(&:*)
--- 2023/06/part2.rb
+++ 2023/06/part2.rb
@@ -1,5 +1,14 @@
+# A more direct way is to solve the equation
+#
+# -speed^2 +/- 7*speed - 9 > 0
+#
+# So we'll use quadratic formula to determine the high and low range
+#
 p STDIN.readlines
   .map { |line| line.split(":").last.delete(" ").to_i }
   .yield_self { |(time, distance)|
-    time.times.map { |speed| [speed, (time-speed)*speed] }.reject { |speed, new_distance| new_distance <= distance }.count
+    # Brute force:
+    # time.times.map { |speed| [speed, (time-speed)*speed] }.reject { |speed, new_distance| new_distance <= distance }.count
+
+    ((-time - Math.sqrt(time**2 - 4*distance))/(-2)).floor + 1 - ((-time + Math.sqrt(time**2 - 4*distance))/(-2)).ceil
   }
