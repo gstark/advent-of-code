8ff9c1b5a749df7ad1f63013fed19ea0bad2bf9d
Gavin Stark <gavin@gstark.com>
2023-12-07 04:53:37 -0500

    2023 07 - refactor part 2 to use direct computation - also format with standardrb

--- 2023/07/part1.rb
+++ 2023/07/part1.rb
@@ -1,21 +1,44 @@
+CARDS = "0123456789TJQKA"
+
+FIVE_KIND = 7
+FOUR_KIND = 6
+FULL_HOUSE = 5
+THREE_KIND = 4
+TWO_PAIR = 3
+ONE_PAIR = 2
+HIGH_CARD = 1
+
 # Could inline this into the one place it is used, but this keeps it more readable. ;)
-def rank_hand(hand)
+def rank(hand)
   sorted = hand.tally.values.sort
-  case
-  when sorted.last == 5         then 7 # five of kind
-  when sorted.last == 4         then 6 # four of kind
-  when sorted == [2,3]          then 5 # full house
-  when sorted.last == 3         then 4 # three of kind
-  when sorted.last(2) == [2, 2] then 3 # two pair
-  when sorted.last == 2         then 2 # one pair
-  else                               1 # high card
+  if sorted.last == 5
+    FIVE_KIND
+  elsif sorted.last == 4
+    FOUR_KIND
+  elsif sorted == [2, 3]
+    FULL_HOUSE
+  elsif sorted.last == 3
+    THREE_KIND
+  elsif sorted.last(2) == [2, 2]
+    TWO_PAIR
+  elsif sorted.last == 2
+    ONE_PAIR
+  else
+    HIGH_CARD
   end
 end
 
-p STDIN.readlines
+p $stdin.readlines
   .map(&:split)
-  .to_h { |cards, bid| [cards.chars.map { |card| "0123456789TJQKA".index(card) }, bid.to_i] }
-  .map { |hand, bid| [rank_hand(hand), hand, bid] }
-  .sort_by { |rank, hand, bid| [rank, hand] }
-  .map.with_index { |(rank, hand, bid), index| bid * (index + 1) }
+  # Turn the cards into their respective value based on their index in the CARDS string
+  # this will give them the correct sorting index. Since we don't care about the actual
+  # representation we can deal with the cards as their value.
+  .map { |cards, bid| [cards.chars.map { |card| CARDS.index(card) }, bid.to_i] }
+  # turn this into an array of the rank of the cards themselves, the cards and the bid
+  .map { |cards, bid| [rank(cards), cards, bid] }
+  # sort them, this sorts by rank first, then by the cards and finally by the bid
+  .sort
+  # now generate their scaled value
+  .map.with_index { |(rank, cards, bid), index| bid * (index + 1) }
+  # sum
   .sum
--- 2023/07/part2.rb
+++ 2023/07/part2.rb
@@ -1,39 +1,79 @@
-def replace_one_joker(array_of_hands)
-  # Turn the array of cards into a new array of cards
-  # by removing a single joker and creating new hands
-  # with all the possible combination
-  array_of_hands.flat_map do |cards|
-    cards.delete_at(cards.index(2))
-    (0..14).reject { |card| card == 2 }.map { |new_card| cards + [new_card] }
-  end
-end
+CARDS = "01J23456789TQKA"
+JOKER_INDEX = CARDS.index("J")
 
-def rank_hand(hand)
-  sorted = hand.tally.values.sort
-  case
-  when sorted.last == 5         then 7 # five of kind
-  when sorted.last == 4         then 6 # four of kind
-  when sorted == [2,3]          then 5 # full house
-  when sorted.last == 3         then 4 # three of kind
-  when sorted.last(2) == [2, 2] then 3 # two pair
-  when sorted.last == 2         then 2 # one pair
-  else                               1 # high card
-  end
-end
+FIVE_KIND = 7
+FOUR_KIND = 6
+FULL_HOUSE = 5
+THREE_KIND = 4
+TWO_PAIR = 3
+ONE_PAIR = 2
+HIGH_CARD = 1
 
+# Could inline this, but this keeps it more readable. ;)
 def rank(hand)
-  all_hands = [hand.dup]
-  while all_hands.any? { |hand| hand.include?(2) }  do
-    all_hands = replace_one_joker(all_hands)
-  end
+  # Split the cards into jokers and non jokers
+  jokers, non_jokers = hand.partition { |card| card == JOKER_INDEX }
 
-  all_hands.map { |hand| rank_hand(hand) }.max
+  if jokers.any?
+    # If there are jokers, rank the non jokers and determine the best
+    # possible hand based on that rank and the number of jokers
+    if jokers.length == 1 && rank(non_jokers) == HIGH_CARD
+      ONE_PAIR
+    elsif jokers.length == 1 && rank(non_jokers) == ONE_PAIR
+      THREE_KIND
+    elsif jokers.length == 1 && rank(non_jokers) == TWO_PAIR
+      FULL_HOUSE
+    elsif jokers.length == 1 && rank(non_jokers) == THREE_KIND
+      FOUR_KIND
+    elsif jokers.length == 1 && rank(non_jokers) == FOUR_KIND
+      FIVE_KIND
+    elsif jokers.length == 2 && rank(non_jokers) == HIGH_CARD
+      THREE_KIND
+    elsif jokers.length == 2 && rank(non_jokers) == ONE_PAIR
+      FOUR_KIND
+    elsif jokers.length == 2 && rank(non_jokers) == THREE_KIND
+      FIVE_KIND
+    elsif jokers.length == 3 && rank(non_jokers) == HIGH_CARD
+      FOUR_KIND
+    elsif jokers.length == 3 && rank(non_jokers) == ONE_PAIR
+      FIVE_KIND
+    else
+      FIVE_KIND
+    end
+  else
+    # Standard ranking, but we may have fewer than five cards
+    # and we won't have any jokers to contend with
+    sorted = non_jokers.tally.values.sort
+
+    if sorted.last == 5
+      FIVE_KIND
+    elsif sorted.last == 4
+      FOUR_KIND
+    elsif sorted == [2, 3]
+      FULL_HOUSE
+    elsif sorted.last == 3
+      THREE_KIND
+    elsif sorted.last(2) == [2, 2]
+      TWO_PAIR
+    elsif sorted.last == 2
+      ONE_PAIR
+    else
+      HIGH_CARD
+    end
+  end
 end
 
-p STDIN.readlines
+p $stdin.readlines
   .map(&:split)
-  .to_h { |cards, bid| [cards.chars.map { |card| "01J23456789TQKA".index(card) }, bid.to_i] }
+  # Turn the cards into their respective value based on their index in the CARDS string
+  # this will give them the correct sorting index. Since we don't care about the actual
+  # representation we can deal with the cards as their value.
+  .map { |cards, bid| [cards.chars.map { |card| CARDS.index(card) }, bid.to_i] }
+  # turn this into an array of the rank of the cards themselves, the cards and the bid
   .map { |cards, bid| [rank(cards), cards, bid] }
-  .sort_by { |rank, cards, bid| [rank, cards] }
+  # sort them, this sorts by rank first, then by the cards and finally by the bid
+  .sort
+  # now generate their scaled value
   .map.with_index { |(rank, cards, bid), index| bid * (index + 1) }
+  # sum
   .sum
