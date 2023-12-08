daeaaaed4197c91f759b13828c00671729c529e1
Gavin Stark <gavin@gstark.com>
2023-12-08 08:55:17 -0500

    2023 08 - another approach to parsing

--- 2023/08/part1.rb
+++ 2023/08/part1.rb
@@ -1,4 +1,5 @@
 p $stdin
-  .readlines(chomp: true)
-  .yield_self { |lines| [lines[0].chars.cycle, lines[2..].map { _1.scan(/\w+/) }.to_h { |source, left, right| [source, {left:, right:}] } ] }
+  .read
+  .split("\n\n")
+  .yield_self { |direction_string, map_lines| [direction_string.chars.cycle, map_lines.split("\n").map { _1.scan(/\w+/) }.to_h { |source, left, right| [source, {left:, right:}] } ] }
   .yield_self { |directions, map| ["AAA"].map { |location| directions.find_index { |direction| (location = (direction == "L") ? map[location][:left] : map[location][:right]) == "ZZZ" } } }.first + 1
--- 2023/08/part2.rb
+++ 2023/08/part2.rb
@@ -1,6 +1,7 @@
 p $stdin
-  .readlines(chomp: true)
-  .yield_self { |lines| [lines[0].chars.cycle, lines[2..].map { _1.scan(/\w+/) }.to_h { |source, left, right| [source, {left:, right:}] } ] }
+  .read
+  .split("\n\n")
+  .yield_self { |direction_string, map_lines| [direction_string.chars.cycle, map_lines.split("\n").map { _1.scan(/\w+/) }.to_h { |source, left, right| [source, {left:, right:}] } ] }
   .yield_self { |directions, map|
     map
       .keys
