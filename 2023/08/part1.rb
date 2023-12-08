48d07fb383f105349d5369c108079bcc5365fa69
Gavin Stark <gavin@gstark.com>
2023-12-08 07:39:26 -0500

    2023 08 - part 1

---
+++ 2023/08/part1.rb
@@ -0,0 +1,9 @@
+p $stdin
+  .readlines(chomp: true)
+  .yield_self { |lines| [lines[0].chars.cycle, lines[2..].to_h { |line| [line[0..2], {left: line[7..9], right: line[12..14]}] }] }
+  .yield_self { |directions, map|
+    current = "AAA"
+    directions.find_index { |direction|
+      (current = (direction == "L") ? map[current][:left] : map[current][:right]) == "ZZZ"
+    }
+  } + 1
