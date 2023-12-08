1c34afff3298fe291315f7d96ebb92ba9d180824
Gavin Stark <gavin@gstark.com>
2023-12-08 10:31:40 -0500

    2023 08 - duh, use 'L' and 'R' for hash keys to simplify lookup

--- 2023/08/part1.rb
+++ 2023/08/part1.rb
@@ -1,5 +1,5 @@
 p $stdin
   .read
   .split("\n\n")
-  .yield_self { |direction_string, map_lines| [direction_string.chars.cycle, map_lines.split("\n").map { _1.scan(/\w+/) }.to_h { |source, left, right| [source, {left:, right:}] } ] }
-  .yield_self { |directions, map| ["AAA"].map { |location| directions.find_index { |direction| (location = (direction == "L") ? map[location][:left] : map[location][:right]) == "ZZZ" } } }.first + 1
+  .yield_self { |direction_string, map_lines| [direction_string.chars.cycle, map_lines.split("\n").map { _1.scan(/\w+/) }.to_h { |source, left, right| [source, {"L" => left, "R" => right}] } ] }
+  .yield_self { |directions, map| ["AAA"].map { |location| directions.find_index { |direction| (location = map[location][direction]) == "ZZZ" } } }.first + 1
--- 2023/08/part2.rb
+++ 2023/08/part2.rb
@@ -1,11 +1,11 @@
 p $stdin
   .read
   .split("\n\n")
-  .yield_self { |direction_string, map_lines| [direction_string.chars.cycle, map_lines.split("\n").map { _1.scan(/\w+/) }.to_h { |source, left, right| [source, {left:, right:}] } ] }
+  .yield_self { |direction_string, map_lines| [direction_string.chars.cycle, map_lines.split("\n").map { _1.scan(/\w+/) }.to_h { |source, left, right| [source, {"L" => left, "R" => right}] } ] }
   .yield_self { |directions, map|
     map
       .keys
       .select { |location| location.end_with?("A") }
-      .map { |location| directions.find_index { |direction| (location = (direction == "L") ? map[location][:left] : map[location][:right]).end_with?("Z") } + 1 }
+      .map { |location| directions.find_index { |direction| (location = map[location][direction]).end_with?("Z") } + 1 }
       .reduce(:lcm)
   }
