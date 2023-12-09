02c1f37f97ab889d955524a3fa10b18168cc2d3e
Gavin Stark <gavin@gstark.com>
2023-12-09 09:11:56 -0500

    2023 09 - formatting

--- 2023/09/part1.rb
+++ 2023/09/part1.rb
@@ -1,15 +1,16 @@
 p $stdin
   .readlines
-  .map { |line| line.split.map(&:to_i) } 
-  .sum { |line| 
+  .map { |line| line.split.map(&:to_i) }
+  .sum { |line|
     (
       result = []
       (0..).map {
         result << line.last
-        line = line.each_cons(2).map { |a,b| b - a }
+        line = line.each_cons(2).map { |a, b| b - a }
         if line.uniq.length == 1
           result << line.last
           break result
         end
       }
-    ).sum }
+    ).sum
+  }
