ba9b6ccd3a26f76e892a2fc443d32b3b934708f6
Gavin Stark <gavin@gstark.com>
2023-12-11 00:48:01 -0500

    2023 11 - part 1

---
+++ 2023/11/part1.rb
@@ -0,0 +1,19 @@
+
+space = $stdin
+  .readlines(chomp: true)
+  .map(&:chars)
+
+width = space[0].length
+height = space.length
+
+double_cols = width.times.map  { |col| height.times.all? { |row| space[row][col] == "." } ? col : nil }.compact
+double_rows = height.times.map { |row| space[row].all? { |spot| spot == "." } ? row : nil }.compact
+
+galaxies = []
+height.times.each do |row|
+  width.times.each do |col|
+    galaxies << [row,col] if space[row][col] == "#"
+  end
+end
+
+p galaxies.permutation(2).map(&:sort).uniq.sum { |g1,g2| (g1[0] - g2[0]).abs + (g1[1] - g2[1]).abs + double_rows.count { |row| Range.new(*[g1[0],g2[0]].sort).include?(row) } + double_cols.count { |col| Range.new(*[g1[1],g2[1]].sort).include?(col) } }
