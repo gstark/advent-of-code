0a31721be5ca7cb481b54a62c2d63c61c1995816
Gavin Stark <gavin@gstark.com>
2023-12-12 15:31:29 -0500

    2023 12 - part 2 - standardrb pass

--- 2023/12/part1.rb
+++ 2023/12/part1.rb
@@ -1,11 +1,9 @@
-
 def possible(line, needed_counts)
   first = line[0...line.index(/#+\?+#*|\?+/)]
   parts = first.scan(/#*/).reject(&:empty?).map(&:length)
   needed_parts = needed_counts.take(parts.length)
-  answer = parts == needed_parts
+  parts == needed_parts
   # puts "Testing if #{line} #{needed_counts} is possible (#{parts} = #{needed_parts}) => #{answer}"
-  answer
 end
 
 def solve(line, needed_counts)
@@ -25,18 +23,18 @@ def solve(line, needed_counts)
     dot_line = line.dup
     dot_line[index] = "."
     # puts "#{line} -> #{new_line} and #{dot_line}"
-    return solve(new_line, needed_counts) + solve(dot_line, needed_counts)
+    solve(new_line, needed_counts) + solve(dot_line, needed_counts)
   else
     new_line = line.dup
     new_line[index] = "."
-    return solve(new_line, needed_counts)
+    solve(new_line, needed_counts)
   end
 end
 
 answer = ARGF
   .readlines(chomp: true)
   .sum do |line, i|
-    lava, needed_counts = line.split(' ')
+    lava, needed_counts = line.split(" ")
     needed_counts = needed_counts.split(",").map(&:to_i)
 
     solve(lava, needed_counts)
