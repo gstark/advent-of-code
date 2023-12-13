ad97452b9501ea16d0ac7e23b56c26a931b2b96e
Gavin Stark <gavin@gstark.com>
2023-12-13 00:43:53 -0500

    2023 12 - part 2 - not my own solution
    Re-implementing an idea from https://github.com/jonathanpaulson
    
    See comments

--- 2023/12/part1.rb
+++ 2023/12/part1.rb
@@ -1,33 +1,80 @@
-def possible(line, needed_counts)
-  first = line[0...line.index(/#+\?+#*|\?+/)]
-  parts = first.scan(/#*/).reject(&:empty?).map(&:length)
-  needed_parts = needed_counts.take(parts.length)
-  parts == needed_parts
-  # puts "Testing if #{line} #{needed_counts} is possible (#{parts} = #{needed_parts}) => #{answer}"
-end
+# This solution is a rework of the solution
+# by: https://github.com/jonathanpaulson
+#
+# It is very creative and very simple. The
+# idea is that we are using recursion but on
+# three things at once, the current dot, the
+# current block index, and finally the length
+# of the # current block itself. If we reach
+# the end of the dots and we've found all the
+# blocks then we have a solution.
+#
+# A cache with the dots_index, blocks_index, and
+# length of the current block is used for optimizing
+# the recursion.
+#
+# One optimization I put in was to simplify the logic
+# by always appending a single "." to the end. This
+# avoids logic for the case where a block ends DIRECTLY
+# at the end of the "dots" string. It removes a conditional
+# and makes the code easier to read.
+#
+# There were also a few "if" conditions in jonathan's code
+# that weren't needed and perhaps were left over from the
+# initial code.
+#
+# The idea is not my own... alas
 
-def solve(line, needed_counts)
-  # puts "Solving #{line}"
-  index = line.index("?")
+CACHE = {}
 
-  if index.nil?
-    scan = line.scan(/#*/).reject(&:empty?).map(&:length) == needed_counts
-    # puts "End of the line: #{line} #{scan}"
-    return scan ? 1 : 0
-  end
+def solve(dots, blocks, dots_index, blocks_index, current_block_length)
+  CACHE[[dots, blocks, dots_index, blocks_index, current_block_length]] ||= begin
+    current_dot = dots[dots_index]
 
-  new_line = line.dup
-  new_line[index] = "#"
+    # If we've reached the end of the line (no current dot) and we've
+    # found all the blocks return 1, otherwise 0
+    if current_dot.nil?
+      return (blocks_index == blocks.length) ? 1 : 0
+    end
 
-  if possible(new_line, needed_counts)
-    dot_line = line.dup
-    dot_line[index] = "."
-    # puts "#{line} -> #{new_line} and #{dot_line}"
-    solve(new_line, needed_counts) + solve(dot_line, needed_counts)
-  else
-    new_line = line.dup
-    new_line[index] = "."
-    solve(new_line, needed_counts)
+    ans = 0
+
+    # We are at a . or a ? (which could be a . if we wanted it to be)
+    if current_dot == "." || current_dot == "?"
+      if current_block_length == 0
+        # If we are not building a block
+        #
+        # add up the number of solutions if we looked at the
+        # next dot index to start the current block
+        #
+        # This handles the case where we consider the current ? to be a "."
+        ans += solve(dots, blocks, dots_index + 1, blocks_index, 0)
+      end
+
+      if current_block_length == blocks[blocks_index]
+        # We are building a block and we found a block that is long enough
+        #
+        # add up the number of solutions if we looked at the
+        # next dot index to start the *NEXT* block by incrementing
+        # the blocks index and resetting the current block length
+        #
+        # This is also considering the current ? to be a "." but where
+        # doing so, ends a current block.
+        ans += solve(dots, blocks, dots_index + 1, blocks_index + 1, 0)
+      end
+    end
+
+    # If we are at "#" or a "?"
+    if current_dot == "#" || current_dot == "?"
+      # In either of these cases we can consider this part of the current block
+      # so we add the number of solutions if we looked at the
+      # next dot index as part of the current block, but incrementing the current block length
+      #
+      # This handles the case where we consider the current ? to be a #
+      ans += solve(dots, blocks, dots_index + 1, blocks_index, current_block_length + 1)
+    end
+
+    ans
   end
 end
 
@@ -37,7 +84,8 @@ answer = ARGF
     lava, needed_counts = line.split(" ")
     needed_counts = needed_counts.split(",").map(&:to_i)
 
-    solve(lava, needed_counts)
+    # Put a "." on the end so we don't have to worry about dangling blocks
+    solve(lava + ".", needed_counts, 0, 0, 0)
   end
 
 p answer
--- 2023/12/part2.rb
+++ 2023/12/part2.rb
@@ -1,25 +1,97 @@
+# This solution is a rework of the solution
+# by: https://github.com/jonathanpaulson
+#
+# It is very creative and very simple. The
+# idea is that we are using recursion but on
+# three things at once, the current dot, the
+# current block index, and finally the length
+# of the # current block itself. If we reach
+# the end of the dots and we've found all the
+# blocks then we have a solution.
+#
+# A cache with the dots_index, blocks_index, and
+# length of the current block is used for optimizing
+# the recursion.
+#
+# One optimization I put in was to simplify the logic
+# by always appending a single "." to the end. This
+# avoids logic for the case where a block ends DIRECTLY
+# at the end of the "dots" string. It removes a conditional
+# and makes the code easier to read.
+#
+# There were also a few "if" conditions in jonathan's code
+# that weren't needed and perhaps were left over from the
+# initial code.
+#
+# The idea is not my own... alas
+
+CACHE = {}
+
+def solve(dots, blocks, dots_index, blocks_index, current_block_length)
+  CACHE[[dots_index, blocks_index, current_block_length]] ||= begin
+    current_dot = dots[dots_index]
+
+    # If we've reached the end of the line (no current dot) and we've
+    # found all the blocks return 1, otherwise 0
+    if current_dot.nil?
+      return (blocks_index == blocks.length) ? 1 : 0
+    end
+
+    ans = 0
+
+    # We are at a . or a ? (which could be a . if we wanted it to be)
+    if current_dot == "." || current_dot == "?"
+      if current_block_length == 0
+        # If we are not building a block
+        #
+        # add up the number of solutions if we looked at the
+        # next dot index to start the current block
+        #
+        # This handles the case where we consider the current ? to be a "."
+        ans += solve(dots, blocks, dots_index + 1, blocks_index, 0)
+      end
+
+      if current_block_length == blocks[blocks_index]
+        # We are building a block and we found a block that is long enough
+        #
+        # add up the number of solutions if we looked at the
+        # next dot index to start the *NEXT* block by incrementing
+        # the blocks index and resetting the current block length
+        #
+        # This is also considering the current ? to be a "." but where
+        # doing so, ends a current block.
+        ans += solve(dots, blocks, dots_index + 1, blocks_index + 1, 0)
+      end
+    end
+
+    # If we are at "#" or a "?"
+    if current_dot == "#" || current_dot == "?"
+      # In either of these cases we can consider this part of the current block
+      # so we add the number of solutions if we looked at the
+      # next dot index as part of the current block, but incrementing the current block length
+      #
+      # This handles the case where we consider the current ? to be a #
+      ans += solve(dots, blocks, dots_index + 1, blocks_index, current_block_length + 1)
+    end
+
+    ans
+  end
+end
+
 answer = ARGF
   .readlines(chomp: true)
   .sum do |line, i|
-    lava, needed_counts = line.split(' ')
+    lava, needed_counts = line.split(" ")
+    needed_counts = needed_counts.split(",").map(&:to_i)
 
     lava = ([lava]*5).join("?")
-    needed_counts = ([needed_counts]*5).join(",")
+    needed_counts = needed_counts * 5
 
-    lava_count = lava.chars.count("?")
-    a = (0..2**lava_count-1).count do |index|
-      replacements = index.to_s(2).rjust(lava_count, '0').tr("10", "#.")
+    # Clear the cache from previous runs
+    CACHE.clear
 
-      needed_counts == lava
-        .gsub(/[?]/)
-        .with_index { |_, position| replacements[position] }
-        .scan(/#*/)
-        .reject(&:empty?)
-        .map(&:length)
-        .join(",")
-    end
-    puts "#{line} #{a}"
-    a
+    # Put a "." on the end so we don't have to worry about dangling blocks
+    solve(lava + ".", needed_counts, 0, 0, 0)
   end
 
 p answer
