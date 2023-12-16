e814034109b36adfe2856ff38727dfa0f4b89d6e
Gavin Stark <gavin@gstark.com>
2023-12-16 11:08:58 -0500

    2023 16 - refactoring

--- 2023/16/part2.rb
+++ 2023/16/part2.rb
@@ -1,64 +1,54 @@
 grid = ARGF.readlines(chomp: true).map(&:chars)
 
+Beam = Data.define(:row, :col, :direction)
+
 def count(grid:, start_row:, start_col:)
-  beams = [{row: start_row, col: start_col, direction: :right}]
+  beams = Set.new([Beam.new(row: start_row, col: start_col, direction: :right)])
   energized = Set.new
-  beam_set = Set.new
+  seen_beams = Set.new
 
   loop do
-    beams = beams.flat_map do |beam|
-      row = beam[:row]
-      col = beam[:col]
-      direction = beam[:direction]
-      current = grid[row][col]
+    beams = beams.reject { |beam| seen_beams.include?(beam) }
 
-      next if beam_set.include?(beam)
-      beam_set << beam
+    beams = beams.flat_map do |beam|
+      row, col, direction = beam.deconstruct
+
+      seen_beams << beam
 
       energized << {row:, col:}
-      case current
+      case grid[row][col]
       when "."
-        case beam[:direction]
-        when :right then {row:, col: col + 1, direction:}
-        when :left then {row:, col: col - 1, direction:}
-        when :up then {row: row - 1, col:, direction:}
-        when :down then {row: row + 1, col:, direction:}
+        case direction
+        when :right then beam.with(col: col + 1)
+        when :left then beam.with(col: col - 1)
+        when :up then beam.with(row: row - 1)
+        when :down then beam.with(row: row + 1)
         end
       when "|"
-        [
-          {row: row - 1, col:, direction: :up},
-          {row: row + 1, col:, direction: :down}
-        ]
+        [beam.with(row: row - 1, direction: :up), beam.with(row: row + 1, direction: :down)]
       when "-"
-        [
-          {row:, col: col - 1, direction: :left},
-          {row:, col: col + 1, direction: :right}
-        ]
+        [beam.with(col: col - 1, direction: :left), beam.with(col: col + 1, direction: :right)]
       when "/"
         case direction
-        when :right then {row: row - 1, col:, direction: :up}
-        when :left then {row: row + 1, col:, direction: :down}
-        when :up then {row:, col: col + 1, direction: :right}
-        when :down then {row:, col: col - 1, direction: :left}
+        when :right then beam.with(row: row - 1, direction: :up)
+        when :left then beam.with(row: row + 1, direction: :down)
+        when :up then beam.with(col: col + 1, direction: :right)
+        when :down then beam.with(col: col - 1, direction: :left)
         end
       when "\\"
         case direction
-        when :right then {row: row + 1, col:, direction: :down}
-        when :left then {row: row - 1, col:, direction: :up}
-        when :up then {row:, col: col - 1, direction: :left}
-        when :down then {row:, col: col + 1, direction: :right}
+        when :right then beam.with(row: row + 1, direction: :down)
+        when :left then beam.with(row: row - 1, direction: :up)
+        when :up then beam.with(col: col - 1, direction: :left)
+        when :down then beam.with(col: col + 1, direction: :right)
         end
       end
-    end.compact.uniq.reject { |beam| beam[:row] < 0 || beam[:col] < 0 || beam[:row] >= grid.length || beam[:col] >= grid[0].length }
+    end
+
+    beams = beams.select { |beam| (0...grid.length).include?(beam.row) && (0...grid[0].length).include?(beam.col) }
 
     return energized.size if beams.empty?
   end
 end
 
-energized_counts = (0...grid.length).flat_map do |start_row|
-  [0, grid[0].length - 1].flat_map do |start_col|
-    count(grid:, start_row:, start_col:)
-  end
-end
-
-p energized_counts.max
+p (0...grid.length).reduce(0) { |max, start_row| [max, count(grid:, start_row:, start_col: 0), count(grid:, start_row:, start_col: grid[0].length - 1)].max }
