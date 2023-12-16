4ce1b8dbc24b1ced14d65de612b2b21b602e18a7
Gavin Stark <gavin@gstark.com>
2023-12-16 10:55:30 -0500

    2023 16 - part 1 - efficient

--- 2023/16/part1.rb
+++ 2023/16/part1.rb
@@ -1,66 +1,58 @@
 grid = ARGF.readlines(chomp: true).map(&:chars)
 
-beams = [
-  {row: 0, col: 0, direction: :right}
-]
+def count(grid:, start_row:, start_col:)
+  beams = [{row: start_row, col: start_col, direction: :right}]
+  energized = Set.new
+  beam_set = Set.new
 
-energized = {}
-seen = {beams: 1, energized: 0}
+  loop do
+    beams = beams.flat_map do |beam|
+      row = beam[:row]
+      col = beam[:col]
+      direction = beam[:direction]
+      current = grid[row][col]
 
-answer = loop do
-  beams = beams.flat_map do |beam|
-    row = beam[:row]
-    col = beam[:col]
-    direction = beam[:direction]
-    current = grid[row][col]
+      next if beam_set.include?(beam)
+      beam_set << beam
 
-    energized[{row:, col:}] = true
-    case current
-    when "."
-      case beam[:direction]
-      when :right then {row:, col: col + 1, direction:}
-      when :left then {row:, col: col - 1, direction:}
-      when :up then {row: row - 1, col:, direction:}
-      when :down then {row: row + 1, col:, direction:}
+      energized << {row:, col:}
+      case current
+      when "."
+        case beam[:direction]
+        when :right then {row:, col: col + 1, direction:}
+        when :left then {row:, col: col - 1, direction:}
+        when :up then {row: row - 1, col:, direction:}
+        when :down then {row: row + 1, col:, direction:}
+        end
+      when "|"
+        [
+          {row: row - 1, col:, direction: :up},
+          {row: row + 1, col:, direction: :down}
+        ]
+      when "-"
+        [
+          {row:, col: col - 1, direction: :left},
+          {row:, col: col + 1, direction: :right}
+        ]
+      when "/"
+        case direction
+        when :right then {row: row - 1, col:, direction: :up}
+        when :left then {row: row + 1, col:, direction: :down}
+        when :up then {row:, col: col + 1, direction: :right}
+        when :down then {row:, col: col - 1, direction: :left}
+        end
+      when "\\"
+        case direction
+        when :right then {row: row + 1, col:, direction: :down}
+        when :left then {row: row - 1, col:, direction: :up}
+        when :up then {row:, col: col - 1, direction: :left}
+        when :down then {row:, col: col + 1, direction: :right}
+        end
       end
-    when "|"
-      [
-        {row: row - 1, col:, direction: :up},
-        {row: row + 1, col:, direction: :down}
-      ]
-    when "-"
-      [
-        {row:, col: col - 1, direction: :left},
-        {row:, col: col + 1, direction: :right}
-      ]
-    when "/"
-      case direction
-      when :right then {row: row - 1, col:, direction: :up}
-      when :left then {row: row + 1, col:, direction: :down}
-      when :up then {row:, col: col + 1, direction: :right}
-      when :down then {row:, col: col - 1, direction: :left}
-      end
-    when "\\"
-      case direction
-      when :right then {row: row + 1, col:, direction: :down}
-      when :left then {row: row - 1, col:, direction: :up}
-      when :up then {row:, col: col - 1, direction: :left}
-      when :down then {row:, col: col + 1, direction: :right}
-      end
-    end
-  end.uniq.reject { |beam| beam[:row] < 0 || beam[:col] < 0 || beam[:row] >= grid.length || beam[:col] >= grid[0].length }
+    end.compact.uniq.reject { |beam| beam[:row] < 0 || beam[:col] < 0 || beam[:row] >= grid.length || beam[:col] >= grid[0].length }
 
-  # (0...grid.length).each do |row|
-  #   (0...grid[0].length).each do |col|
-  #     print energized[{row:, col:}] ? "#" : grid[row][col]
-  #   end
-  #   puts
-  # end
-  if seen.key?({beams:, energized:})
-    break energized.size
+    return energized.size if beams.empty?
   end
-
-  seen[{beams:, energized:}] = true
 end
 
-p answer
+p count(grid:, start_row: 0, start_col: 0)
