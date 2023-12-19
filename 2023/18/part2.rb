4a5d7214b9b6f3081ad7cfbe96be70acfb7debf8
Gavin Stark <gavin@gstark.com>
2023-12-18 21:30:16 -0500

    2023 18 - Day 2 - Shoelaces and Pick Theorem for fast solve

--- 2023/18/part2.rb
+++ 2023/18/part2.rb
@@ -1,7 +1,8 @@
 plan = ARGF
   .readlines(chomp: true)
   .map(&:split)
-  .map { |direction, length| [direction, length.to_i] }
+  # Map special parsing of input
+  .map { |direction, _, color| [{"0" => "R", "1" => "D", "2" => "L", "3" => "U"}[color[-2]], color[2..6].to_i(16)] }
 
 # Lookup table
 DIRS = {
@@ -23,7 +24,7 @@ end
 area = 0.5 * (1...coords.length - 1).sum { |i| coords[i][:x] * (coords[i + 1][:y] - coords[i - 1][:y]) }.abs
 
 # Picks theorem solved for interior points: https://en.wikipedia.org/wiki/Pick%27s_theorem
-boundary = plan.sum { |_, length| length }
+boundary = plan.sum { |_, length, _| length }
 interior = area - boundary / 2 + 1
 
 # Add the interior plus the boundary
