64ff2017b93d534bb0c97912b9b4d5eea1b06c30
Gavin Stark <gavin@gstark.com>
2023-12-18 21:54:53 -0500

    Oops, inverted direction for up and down, good thing it still worked, just inverted

--- 2023/18/part1.rb
+++ 2023/18/part1.rb
@@ -5,8 +5,8 @@ plan = ARGF
 
 # Lookup table
 DIRS = {
-  "D" => {x: 0, y: -1},
-  "U" => {x: 0, y: 1},
+  "D" => {x: 0, y: 1},
+  "U" => {x: 0, y: -1},
   "L" => {x: -1, y: 0},
   "R" => {x: 1, y: 0}
 }
@@ -19,6 +19,8 @@ plan.each do |direction, length|
   coords << {x: coords.last[:x] + DIRS[direction][:x] * length, y: coords.last[:y] + DIRS[direction][:y] * length}
 end
 
+p coords
+
 # Shoelace formula for area: https://en.wikipedia.org/wiki/Shoelace_formula
 area = 0.5 * (1...coords.length - 1).sum { |i| coords[i][:x] * (coords[i + 1][:y] - coords[i - 1][:y]) }.abs
 
@@ -27,4 +29,5 @@ boundary = plan.sum { |_, length| length }
 interior = area - boundary / 2 + 1
 
 # Add the interior plus the boundary
+p({area:, boundary:, interior:})
 p (interior + boundary).to_i
