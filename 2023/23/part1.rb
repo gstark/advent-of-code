a3995158fbff50fda596277c105034783b43ba0c
Gavin Stark <gavin@gstark.com>
2023-12-23 13:26:24 -0500

    2023 23 - Part 1 - Works, but is not as efficient as it could be

---
+++ 2023/23/part1.rb
@@ -0,0 +1,38 @@
+require_relative "astar"
+
+grid = ARGF.readlines(chomp: true).map(&:chars)
+
+height = grid.length
+width = grid[0].length
+
+start_row = 0
+start_col = 1
+target_row = height - 1
+target_col = width - 2
+queue = [[start_row, start_col, Set.new]]
+
+walks = []
+while queue.any?
+  row, col, path = queue.pop
+
+  if [row, col] == [target_row, target_col]
+    walks << path.length
+  end
+
+  directions = case grid[row][col]
+  when ">" then [[0, 1]]
+  when "<" then [[0, -1]]
+  when "v" then [[1, 0]]
+  when "^" then [[-1, 0]]
+  else [[0, -1], [0, 1], [-1, 0], [1, 0]]
+  end
+
+  directions
+    .map { |dr, dc| [row + dr, col + dc] }
+    .select { |row, col| row >= 0 && col >= 0 && row < grid.length && col < grid[0].length }
+    .select { |row, col| grid[row][col] != "#" }
+    .select { |row, col| !path.include?([row,col]) }
+    .each { |row, col| queue << [row, col, Set.new([*path, [row,col]])] }
+end
+
+p walks.max
