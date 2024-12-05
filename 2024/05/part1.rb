892daae7ccbf4883d3358f5bed425486a9b74518
Gavin Stark <gavin@gstark.com>
2024-12-05 16:40:28 -0500

    Day 05 - Figured out how to one-liner part 2 and reformatted part 1 as a single line

--- 2024/05/part1.rb
+++ 2024/05/part1.rb
@@ -4,13 +4,5 @@ p $stdin
   .map(&:to_a)
   .yield_self { |rules, updates| [rules[0...-1], updates] }
   .yield_self { |rules, updates| [rules.map { |rule| rule.split("|").map(&:to_i) }, updates.map { |update| update.split(",").map(&:to_i) }] }
-  .yield_self { |rules, updates|
-    updates.filter { |update|
-      update.all? { |page|
-        rules
-          .filter { |a, _| a == page }
-          .all? { |a, b| !update.include?(b) || update.index(a) < update.index(b) }
-      }
-    }
-  }
+  .yield_self { |rules, updates| updates.filter { |update| update.all? { |page| rules .filter { |a, _| a == page }.all? { |a, b| !update.include?(b) || update.index(a) < update.index(b) } } } }
   .sum { |update| update[update.length / 2] }
--- 2024/05/part2.rb
+++ 2024/05/part2.rb
@@ -5,20 +5,6 @@ p $stdin
   .yield_self { |rules, updates| [rules[0...-1], updates] }
   .yield_self { |rules, updates| [rules.map { |rule| rule.split("|").map(&:to_i) }, updates.map { |update| update.split(",").map(&:to_i) }] }
   .yield_self { |rules, updates| [rules.sort_by { |a, _| a }, updates] }
-  .yield_self { |rules, updates| [rules, updates.reject { |update| update.all? { |page| rules.filter { |a, _| a == page }.all? { |a, b| !update.include?(b) || update.index(a) < update.index(b) } }}] }
-  .yield_self { |rules, updates|
-    updates.map { |update|
-      update.tap { |new_update|
-        rules.each { |a, b|
-          ai = new_update.index(a)
-          bi = new_update.index(b)
-
-          if ai && bi && ai > bi
-            new_update.delete_at(ai)
-            new_update.insert(bi, a)
-          end
-        }
-      }
-    }
-  }
+  .yield_self { |rules, updates| [rules, updates.reject { |update| update.all? { |page| rules.filter { |a, _| a == page }.all? { |a, b| !update.include?(b) || update.index(a) < update.index(b) } } }] }
+  .yield_self { |rules, updates| updates.map { |update| rules.reduce(update) { |new_update, (a, b)| (new_update.index(a) && new_update.index(b) && new_update.index(a) > new_update.index(b)) ? new_update.insert(new_update.index(b), new_update.delete_at(new_update.index(a))) : new_update } } }
   .sum { |update| update[update.length / 2] }
